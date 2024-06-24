cebce3b93bd7c41666426ee38ce246dd9e881d9f
gratefulvortex <168163647+gratefulvortex@users.noreply.github.com>
2024-06-24 14:54:20 +0530

    Delete P.DJANGO/myproject directory

--- P.DJANGO/myproject/gym/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Membership
-
-class MembershipForm(forms.ModelForm):
-    class Meta:
-        model = Membership
-        fields = ['name', 'last_name', 'mobile_number', 'email', 'age', 'gender', 'membership_plan', 'tenure', 'payment_status', 'start_date', 'end_date']
--- P.DJANGO/myproject/myproject/urls.py
+++
@@ -1,8 +0,0 @@
-# Django project's urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('gym.urls')),  # Include your app's URLs
-]
